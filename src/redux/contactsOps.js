f1f735fbeab1fd3652302400ec13a82afa42317e
tochechka89 <rabota20ton@gmail.com>
2024-08-13 20:47:17 +0300

    contactsOps, contactsSlice

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,36 +1,31 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://66a40d4644aa637045833ecd.mockapi.io/'
+axios.defaults.baseURL = 'https://66a40d4644aa637045833ecd.mockapi.io/';
 
 export const fetchContacts = createAsyncThunk('contacts/fetchContacts', async (_, thunkAPI) => {
     try {
-        const response = await axios.get('contacts');
+        const response = await axios.get('/contacts');
         return response.data;
     } catch (error) {
-        return thunkAPI.rejectWithValue('Error fetching data!');
+        return thunkAPI.rejectWithValue(error.message || 'Ошибка при получении контактов!');
     }
-
 });
 
-
-export const fetchNewContact = createAsyncThunk('contacts/fetchNewContact', async (newContact, thunkAPI) => {
+export const addContact = createAsyncThunk('contacts/addContact', async (newContact, thunkAPI) => {
     try {
-        const response = await axios.post('contacts', newContact);
+        const response = await axios.post('/contacts', newContact);
         return response.data;
     } catch (error) {
-        return thunkAPI.rejectWithValue('Error fetching data!');
+        return thunkAPI.rejectWithValue(error.message || 'Ошибка при добавлении контакта!');
     }
-
 });
-
 
 export const deleteContact = createAsyncThunk('contacts/deleteContact', async (id, thunkAPI) => {
     try {
-        const response = await axios.delete(`contacts/${id}`);
-        return response.data;
+        await axios.delete(`/contacts/${id}`);
+        return id; 
     } catch (error) {
-        return thunkAPI.rejectWithValue('Error fetching data!');
+        return thunkAPI.rejectWithValue(error.message || 'Ошибка при удалении контакта!');
     }
-
 });
